f8a9cce716a492885aed43b39f0245b361b8aeb8
Oleh Karpovych <oleg.karpovich.94@gmail.com>
2025-09-23 18:18:03 +0300

    feat: UI corrections

--- src/components/AppFooter/index.tsx
+++ src/components/AppFooter/index.tsx
@@ -6,9 +6,9 @@ const AppFooter: FC = () => {
   return (
     <footer className="w-full">
       <div className="w-full flex justify-center items-center bg-background">
-        <div className="flex justify-between w-full lg:w-9/12 py-4 px-6 lg:px-0">
-          <p className="font-light uppercase tracking-tighter bg-gradient-to-r from-gray-900 to-gray-600 dark:from-gray-100 dark:to-gray-400 text-transparent bg-clip-text">Tennis store</p>
-          <p className="font-light">© {currentYear}, Tennis Store, All right reserved.</p>
+        <div className="flex justify-between items-center w-full lg:w-9/12 py-4 px-6 lg:px-0">
+          <p className="font-light text-xs md:text-normal uppercase tracking-tighter bg-gradient-to-r from-gray-900 to-gray-600 dark:from-gray-100 dark:to-gray-400 text-transparent bg-clip-text">Tennis store</p>
+          <p className="font-light text-xs md:text-sm">© {currentYear}, Tennis Store, All right reserved.</p>
         </div>
       </div>
     </footer>
--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -5,27 +5,32 @@ import MainNav from "@/components/AppHeader/MainNav";
 import HeaderSearch from "@/components/AppHeader/HeaderSearch";
 import { Button } from "@/components/ui/button";
 import { ShoppingCart } from "lucide-react"
+import Link from "next/link";
 
 
 const AppHeader: FC = () => {
   return (
     <header className="w-full flex flex-col justify-center items-center">
       <div className="w-full flex justify-center items-center bg-background">
-        <div className="flex justify-between w-full gap-2 lg:w-9/12 py-3 px-6 lg:px-0">
-          <div className="text-4xl font-light uppercase tracking-tighter bg-gradient-to-r from-gray-900 to-gray-600 dark:from-gray-100 dark:to-gray-400 text-transparent bg-clip-text">
-            Tennis Store
+        <div className="flex justify-between flex-col sm:flex-row w-full gap-2 lg:w-9/12 py-3 px-6 lg:px-0">
+          <div className="text-2xl md:text-3-xl font-light flex justify-center sm:justify-start uppercase tracking-tighter bg-gradient-to-r from-gray-900 to-gray-600 dark:from-gray-100 dark:to-gray-400 text-transparent bg-clip-text">
+            <Link href="/">
+              Tennis Store
+            </Link>
           </div>
-          <div className="w-2/5 flex items-center">
-            <HeaderSearch />
-          </div>
-          <div className="flex items-center gap-2">
-            <ThemeProvider>
-              <ThemeSwitcher />
-            </ThemeProvider>
-            <Button className="bg-destructive hover:bg-destructive-light cursor-pointer font-light text-white">
-              <ShoppingCart />
-              <span>€0</span>
-            </Button>
+          <div className="flex justify-between gap-2">
+            <div className="w-full flex items-center">
+              <HeaderSearch />
+            </div>
+            <div className="flex items-center gap-2">
+              <ThemeProvider>
+                <ThemeSwitcher />
+              </ThemeProvider>
+              <Button variant="destructive">
+                <ShoppingCart strokeWidth={1} />
+                <span>€0</span>
+              </Button>
+            </div>
           </div>
         </div>
       </div>
