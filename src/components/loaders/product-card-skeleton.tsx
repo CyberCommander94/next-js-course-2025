8061bddea099324602d679f59cc60bab5da3de2c
Oleh Karpovych <oleg.karpovich.94@gmail.com>
2025-09-27 10:26:29 +0300

    fix:  UI corrections

--- src/components/loaders/product-card-skeleton.tsx
+++ src/components/loaders/product-card-skeleton.tsx
@@ -4,7 +4,7 @@ import { Skeleton } from "@/components/ui/skeleton"
 const ProductCardSkeleton: FC = () => {
   return (
     <div className="w-full p-4 border border-gray-200 dark:border-accent bg-transparent">
-      <Skeleton className="w-full h-40 bg-gray-200 dark:bg-accent rounded-none mb-4" />
+      <Skeleton className="w-full h-50 bg-gray-200 dark:bg-accent rounded-none mb-4" />
       <div className="flex gap-2 w-full h-[50px]">
         <div className="flex flex-col gap-1 h-full w-full">
           <Skeleton className="h-[20px] w-full bg-gray-200 dark:bg-accent rounded-none mb-1" />
--- src/components/pages/home/top-10-rackets-list/index.tsx
+++ src/components/pages/home/top-10-rackets-list/index.tsx
@@ -18,7 +18,7 @@ const Top10RacketsList: FC<Top10racketsListProps> = ({ rackets }) => {
   return (
     <section className="flex flex-col items-center w-full h-full">
       <div className="flex justify-end items-center w-full">
-        <Link href="/rackets/top-10" className="px-2 py-2 bg-transparent border border-foreground dark:border text-center hover:text-white hover:bg-foreground dark:text-white dark:border-input dark:hover:bg-input/50">
+        <Link href="/top-10" className="px-2 py-2 bg-transparent border border-foreground dark:border text-center hover:text-white hover:bg-foreground dark:text-white dark:border-input dark:hover:bg-input/50">
           <span className="text-sm font-light">См. Все</span>
         </Link>
       </div>
