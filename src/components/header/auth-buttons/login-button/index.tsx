afae3cd59dff78b174c5629b93e8e4f1899538f2
Oleh Karpovych <oleg.karpovich.94@gmail.com>
2025-10-06 11:39:55 +0300

    fix: 403 page file naming, update login button

--- src/components/header/auth-buttons/login-button/index.tsx
+++ src/components/header/auth-buttons/login-button/index.tsx
@@ -1,24 +1,17 @@
 'use client';
 
 import { FC } from 'react';
-import { Button } from '@/components/ui/button';
 import { LogIn } from 'lucide-react';
-import { useRouter } from 'next/navigation';
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
+import Link from 'next/link';
 
 const LoginButton: FC = () => {
-  const router = useRouter();
-
-  const handleClick = () => {
-    router.push(`/login`);
-  };
-
   return (
     <Tooltip>
       <TooltipTrigger asChild>
-        <Button onClick={handleClick}>
+        <Link href="/login" className="h-9 px-4 py-2 has-[>svg]:px-3 dark:bg-transparent dark:hover:bg-input hover:bg-foreground border-foreground dark:border-input text-foreground hover:text-white dark:text-white bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 focus-visible:outline-none border font-light focus-visible:ring-0 inline-flex cursor-pointer rounded-none items-center justify-center gap-2 whitespace-nowrap text-sm transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none">
           <LogIn strokeWidth={1} />
-        </Button>
+        </Link>
       </TooltipTrigger>
       <TooltipContent>
         <p>Войти в систему</p>
